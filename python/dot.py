efa7e5b7522714246162f3645bd6584b293d8797
DongjuPark <pdj95044@gmail.com>
2023-01-03 01:24:50 +0900

    solve problem

--- python/dot.py
+++ python/dot.py
@@ -1,4 +1,40 @@
 #1/2
+# y = x기준으로 똑같다는 특징을 이용 한쪽 부분만 비교.. 이것 또한 시간 복잡도...
+from itertools import combinations_with_replacement as cwr
+
+def smallThanP(i,j,dMax):
+    return dMax >= i ** 2 + j ** 2
+
+def getP(k,d,pList):
+    a = 0
+    while k*a <= d:
+        pList.append(k*a)
+        a += 1
+    return pList
+
+def solution(k, d):
+    answer = 0
+    pList = []
+    pList = getP(k,d,pList)
+    pList = list(cwr(pList,2))
+    dMax = d ** 2
+    
+    for case in pList:
+        i,j = case
+        if(i == j):
+            if smallThanP(i,j,dMax) == True:
+                answer += 1
+        else:
+            if smallThanP(i,j,dMax) == True:
+                answer += 2
+    
+    return answer
+
+print(solution(2,4))
+
+"""
+전체 경우의 수를 비교 시간 복잡도 인해 실패...
+
 from itertools import product
 
 def getRangeOfDot(k,d,rangeOfDot):
@@ -27,5 +63,4 @@ def solution(k, d):
             cnt += 1
 
     return cnt
-
-print(solution(1000,10000))
+"""
